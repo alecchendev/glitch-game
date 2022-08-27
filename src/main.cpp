7e6d08d91eef161933780f3961a456a456ba85c2
Alec Chen <alecchendev@gmail.com>
2022-08-27 14:23:27 -0500

    Draw many cubes

--- src/main.cpp
+++ src/main.cpp
@@ -215,10 +215,24 @@ int main() {
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
+		// Cube positions
+		glm::vec3 cubePositions[] = {
+			glm::vec3( 0.0f,  0.0f,  0.0f), 
+			glm::vec3( 2.0f,  5.0f, -15.0f), 
+			glm::vec3(-1.5f, -2.2f, -2.5f),  
+			glm::vec3(-3.8f, -2.0f, -12.3f),  
+			glm::vec3( 2.4f, -0.4f, -3.5f),  
+			glm::vec3(-1.7f,  3.0f, -7.5f),  
+			glm::vec3( 1.3f, -2.0f, -2.5f),  
+			glm::vec3( 1.5f,  2.0f, -2.5f), 
+			glm::vec3( 1.5f,  0.2f, -1.5f), 
+			glm::vec3(-1.3f,  1.0f, -1.5f)  
+		};
+
 		// Coordinate system
-		glm::mat4 model = glm::mat4(1.0f);
+		// glm::mat4 model = glm::mat4(1.0f);
 		// model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); 
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+		// model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
 		glm::mat4 view = glm::mat4(1.0f);
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
 		glm::mat4 projection;
@@ -227,14 +241,29 @@ int main() {
 		shader_program.use();
 
 		// Set uniforms for perspective
-		glUniformMatrix4fv(glGetUniformLocation(shader_program.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(glGetUniformLocation(shader_program.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(glGetUniformLocation(shader_program.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		// glUniformMatrix4fv(glGetUniformLocation(shader_program.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		// glUniformMatrix4fv(glGetUniformLocation(shader_program.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		// glUniformMatrix4fv(glGetUniformLocation(shader_program.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		shader_program.setMat4("view", view);
+		shader_program.setMat4("projection", projection);
 
 		// Get vertices and draw
-		glBindVertexArray(VAO);
+		// glBindVertexArray(VAO);
 		// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		// glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glBindVertexArray(VAO);
+		for(unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i; 
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+			shader_program.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
